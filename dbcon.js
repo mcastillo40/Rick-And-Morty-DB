384c9cc4259eab6863b6ef75e68e5a2602816a1b
Matthew C <chgreen08@gmail.com>
2017-11-22 21:52:30 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_castimat',
-  password        : '5321',
+  password        : 'xxxx',
   database        : 'cs340_castimat'
 });
 module.exports.pool = pool;
